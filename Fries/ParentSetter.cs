ba8539907613a02a8c90bea78703d3c7ed87eab9
Ghostfish21 <43630149+Ghostfish21@users.noreply.github.com>
2024-12-01 21:16:09 -0500

    创建了 ParentSetter 类

---
+++ Fries/ParentSetter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace Fries {
+    public class ParentSetter : MonoBehaviour {
+        public Transform parent;
+        
+        private void Start() {
+            transform.SetParent(parent);
+            Destroy(this);
+        }
+    }
+}
