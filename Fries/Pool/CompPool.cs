98dd5ffbb79fa2732aa8f22c5f8c9900aee5da6d
Ghostfish21 <43630149+Ghostfish21@users.noreply.github.com>
2024-12-04 20:19:15 -0500

    1

--- Fries/Pool/CompPool.cs
+++ Fries/Pool/CompPool.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 namespace Fries.Pool {
     public class CompPool<T> : APool<T> where T : Component {
         
+        public CompPool(Action<T> compSetup, Action<T> resetter, Transform root, GameObject prefab, int size = 5) : base(() => {
+            T comp = GameObject.Instantiate(prefab, root).GetComponent<T>();
+            comp.gameObject.SetActive(false);
+            compSetup(comp);
+            return comp;
+        }, size) {
+            this.resetter = resetter;
+        }
+        
         public CompPool(Action<T> resetter, Transform root, GameObject prefab, int size = 5) : base(() => {
             T comp = GameObject.Instantiate(prefab, root).GetComponent<T>();
             comp.gameObject.SetActive(false);
--- Fries/Pool/PoolExt.cs
+++ Fries/Pool/PoolExt.cs
@@ -5,5 +5,6 @@ namespace Fries.Pool {
     public static class PoolExt {
         public static CompPool<T> toPool<T>(this GameObject prefab, Transform root, int size = 5) where T : Component => new(root, prefab, size);
         public static CompPool<T> toPool<T>(this GameObject prefab, Action<T> resetter, Transform root, int size = 5) where T : Component => new(resetter, root, prefab, size);
+        public static CompPool<T> toPool<T>(this GameObject prefab, Action<T> compSetup, Action<T> resetter, Transform root, int size = 5) where T : Component => new(compSetup, resetter, root, prefab, size);
     }
 }
