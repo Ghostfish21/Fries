d1a09c18faf4fd7422dd4601037660f6f6a5b703
Ghostfish21 <43630149+Ghostfish21@users.noreply.github.com>
2025-01-28 21:40:06 -0500

    Update SelectableInputTypeDrawer.cs

--- Fries/Inspector/SelectableInputTypeDrawer.cs
+++ Fries/Inspector/SelectableInputTypeDrawer.cs
@@ -2,6 +2,7 @@
 using UnityEditor;
 using UnityEngine;
 using System.Collections.Generic;
+using System.Reflection;
 
 namespace Fries.Inspector {
     [CustomPropertyDrawer(typeof(SelectableInputType))]
@@ -9,11 +10,12 @@ namespace Fries.Inspector {
         private const float PopupWidth = 120f;
         private const float Spacing = 2f;
         private List<SerializedProperty> inputProps;
+        private MonoBehaviour target;
 
         private void Init(SerializedProperty serializedProperty) {
             inputProps = new List<SerializedProperty>();
             SerializedProperty inputPropsSP = serializedProperty.FindPropertyRelative("inputProperties");
-            var target = serializedProperty.FindPropertyRelative("target").objectReferenceValue as MonoBehaviour;
+            target = serializedProperty.FindPropertyRelative("target").objectReferenceValue as MonoBehaviour;
             for (int i = 0; i < inputPropsSP.arraySize; i++) {
                 string propertyName = inputPropsSP.GetArrayElementAtIndex(i).stringValue;
                 inputProps.Add(getSerializedProperty(target, propertyName));
@@ -66,11 +68,22 @@ namespace Fries.Inspector {
 
             // 绘制当前选中的属性
             if (selectedIndex.intValue >= 0 && selectedIndex.intValue < inputProperties.arraySize) {
+                EditorGUI.BeginChangeCheck();
                 SerializedProperty selectedProperty = inputProps[selectedIndex.intValue];
                 EditorGUI.PropertyField(valueRect, selectedProperty, GUIContent.none);
+                if (EditorGUI.EndChangeCheck()) {
+                    object value = GetValue(selectedProperty);
+                    string propertyName = inputProperties.GetArrayElementAtIndex(selectedIndex.intValue).stringValue;
+                    setValue(propertyName, value);
+                }
             }
 
             EditorGUI.EndProperty();
+        }
+
+        private void setValue(string propertyName, object value) {
+            FieldInfo fieldInfo = target.GetType().GetField(propertyName);
+            fieldInfo.SetValue(target, value);
         }
 
         private void DrawTypePopup(Rect rect, SerializedProperty selectedIndex, SerializedProperty inputTypes) {
@@ -90,6 +103,51 @@ namespace Fries.Inspector {
 
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
             return EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
+        }
+
+        private object GetValue(SerializedProperty property) {
+            switch (property.propertyType) {
+                case SerializedPropertyType.Integer:
+                    return property.intValue;
+                case SerializedPropertyType.Boolean:
+                    return property.boolValue;
+                case SerializedPropertyType.Float:
+                    return property.floatValue;
+                case SerializedPropertyType.String:
+                    return property.stringValue;
+                case SerializedPropertyType.Color:
+                    return property.colorValue;
+                case SerializedPropertyType.ObjectReference:
+                    return property.objectReferenceValue;
+                case SerializedPropertyType.LayerMask:
+                    return property.intValue;
+                case SerializedPropertyType.Enum:
+                    return property.enumValueIndex;
+                case SerializedPropertyType.Vector2:
+                    return property.vector2Value;
+                case SerializedPropertyType.Vector3:
+                    return property.vector3Value;
+                case SerializedPropertyType.Vector4:
+                    return property.vector4Value;
+                case SerializedPropertyType.Rect:
+                    return property.rectValue;
+                case SerializedPropertyType.ArraySize:
+                    return property.arraySize;
+                case SerializedPropertyType.Character:
+                    return (char)property.intValue;
+                case SerializedPropertyType.AnimationCurve:
+                    return property.animationCurveValue;
+                case SerializedPropertyType.Bounds:
+                    return property.boundsValue;
+                case SerializedPropertyType.Quaternion:
+                    return property.quaternionValue;
+
+                // 如果有更多类型，可以继续添加
+
+                default:
+                    Debug.LogWarning($"未处理的 SerializedProperty 类型: {property.propertyType}");
+                    return null;
+            }
         }
     }
 }
