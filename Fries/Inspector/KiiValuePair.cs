f64415fbc8b5a87e045247de025bea7b93dc8c4a
Ghostfish21 <43630149+Ghostfish21@users.noreply.github.com>
2025-02-05 12:50:38 -0500

    Update KiiValuePair.cs

--- Fries/Inspector/KiiValuePair.cs
+++ Fries/Inspector/KiiValuePair.cs
@@ -1,15 +1,30 @@
-﻿using UnityEngine;
+﻿using System.IO;
+using UnityEngine;
 
 namespace Fries.Inspector {
+    # if UNITY_EDITOR
+    using UnityEditor;
+    # endif
+    
     [System.Serializable]
     public class KiiValuePair {
-        public float keyPercentage;
-        public float valuePercentage;
+        public KiiValuePair(float keyWidth = 0, float valueWidth = 0) {
+            string dataPath = Application.dataPath;
+            // 获取Assets文件夹所在的父文件夹名称，即项目名称
+            string projectName = new DirectoryInfo(dataPath).Parent.Name;
+            Debug.Log("Project Name: " + projectName);
+            # if UNITY_EDITOR
+            EditorPrefs.SetFloat($"{projectName}.{this.GetType().Name}.Key_Width", keyWidth);
+            EditorPrefs.SetFloat($"{projectName}.{this.GetType().Name}.Value_Width", valueWidth);
+            # endif
+        }
     }
 
     [System.Serializable]
     public class KiiValuePair<K, V> : KiiValuePair { 
         [SerializeField] public K key;
         [SerializeField] public V value;
+
+        public KiiValuePair(float keyWidth = 0, float valueWidth = 0) : base(keyWidth, valueWidth) { }
     }
 }
