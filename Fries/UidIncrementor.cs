d4dda6402051203cec009315642fd87fd55ae868
Ghostfish21 <43630149+Ghostfish21@users.noreply.github.com>
2025-02-04 20:44:11 -0500

    更新uid生成器

---
+++ Fries/UidIncrementor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Fries {
+    public class UidIncrementor {
+        private ulong uid = 0;
+
+        public UidIncrementor(ulong initUid = 0) {
+            this.uid = initUid;
+        }
+
+        private ulong GetUid() {
+            uid++;
+            return uid;
+        }
+
+    }
+}
