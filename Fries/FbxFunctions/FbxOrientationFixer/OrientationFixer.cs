8218217e3057f5997a7444c494719888b90db309
Ghostfish21 <43630149+Ghostfish21@users.noreply.github.com>
2025-02-20 20:41:11 -0500

    Update OrientationFixer.cs

--- Fries/FbxFunctions/FbxOrientationFixer/OrientationFixer.cs
+++ Fries/FbxFunctions/FbxOrientationFixer/OrientationFixer.cs
@@ -48,7 +48,7 @@ namespace Fries.FbxFunctions.FbxOrientationFixer {
                         // 如果路径以 "Assets" 开头，则用 Application.dataPath 替换 Assets 部分
                         if (relativePath.StartsWith("Assets")) 
                             absolutePath = Application.dataPath + relativePath.Substring("Assets".Length);
-                        if (relativePath.StartsWith("Package")) 
+                        if (relativePath.StartsWith("Packages")) 
                             absolutePath = Path.GetFullPath(Path.Combine(Directory.GetParent(Application.dataPath).FullName, relativePath));
                         else absolutePath = Path.GetFullPath(relativePath);
                         absolutePath = $"\"{absolutePath}\"";
