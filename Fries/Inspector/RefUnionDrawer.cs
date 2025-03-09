d8500ff0897ccebe4dc6971422c59cc776c17b15
Ghostfish21 <43630149+Ghostfish21@users.noreply.github.com>
2025-03-09 00:02:53 -0500

    BUGFIX

--- Fries/Inspector/RefUnionDrawer.cs
+++ Fries/Inspector/RefUnionDrawer.cs
@@ -54,8 +54,7 @@ namespace Fries.Inspector {
             // 绘制当前选中的属性
             if (selectedIndex.intValue >= 0 && selectedIndex.intValue < maxIndex) {
                 EditorGUI.BeginChangeCheck();
-                SerializedProperty selectedProperty = property.serializedObject.FindProperty(
-                    AnInspector.getCachedPropertyPath(target, inputObjRefs.GetArrayElementAtIndex(selectedIndex.intValue).getValue()));
+                SerializedProperty selectedProperty = property.serializedObject.FindProperty(inputObjRefs.GetArrayElementAtIndex(selectedIndex.intValue).propertyPath);
                 EditorGUI.PropertyField(valueRect, selectedProperty, GUIContent.none);
                 if (EditorGUI.EndChangeCheck()) {
                     object value = selectedProperty.getValue(); 
