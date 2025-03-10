8f6eb4b0f140cdaa1d578aae588decb5ae3dfe5d
Ghostfish21 <43630149+Ghostfish21@users.noreply.github.com>
2025-03-10 18:46:28 -0400

    Update StringInputWindow.cs

--- Fries/Inspector/StringInputWindow.cs
+++ Fries/Inspector/StringInputWindow.cs
@@ -29,10 +29,23 @@ namespace Fries.Inspector {
         }
 
         private void OnGUI() {
-            // 显示输入框，预填原有的字符串值
-            int lineCount = Mathf.Max(1, inputText.Split('\n').Length);
-            float textAreaHeight = EditorGUIUtility.singleLineHeight * lineCount + 10;
-            inputText = EditorGUILayout.TextArea(inputText, GUILayout.Height(textAreaHeight));
+            GUIStyle textAreaStyle = EditorStyles.textArea;
+            // 先将文本按换行符分割，计算每一行的宽度，取最大值
+            string[] lines = inputText.Split('\n');
+            float maxLineWidth = 0;
+            foreach (string line in lines) {
+                Vector2 lineSize = textAreaStyle.CalcSize(new GUIContent(line));
+                if (lineSize.x > maxLineWidth)
+                    maxLineWidth = lineSize.x;
+            }
+            // 为宽度添加一点边距
+            float horizontalMargin = 20f;
+            float textAreaWidth = maxLineWidth + horizontalMargin;
+            // 根据计算出的宽度获取合适的高度
+            // 注意这里 CalcHeight 会根据文本在给定宽度下的换行情况返回需要的高度
+            float textAreaHeight = textAreaStyle.CalcHeight(new GUIContent(inputText), textAreaWidth) + 10f;
+            // 绘制 TextArea，并使用计算出的宽度和高度
+            inputText = EditorGUILayout.TextArea(inputText, textAreaStyle, GUILayout.Width(textAreaWidth), GUILayout.Height(textAreaHeight));
 
             // 提交按钮
             if (GUILayout.Button("Submit")) {
@@ -46,7 +59,7 @@ namespace Fries.Inspector {
                 // 例如，这里取最后一个布局元素的 Rect，并加上一些额外的 margin
                 Rect contentRect = GUILayoutUtility.GetLastRect();
                 // 如果内容 Rect 过小，设置一个最小尺寸
-                float width = 300;
+                float width = contentRect.x;
                 float height = contentRect.y + EditorGUIUtility.singleLineHeight;
                 // 设置窗口最小和最大尺寸为同样的值，达到固定窗口大小的目的
                 this.minSize = new Vector2(width, height);
