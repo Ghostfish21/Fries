cf01c4843a2344db368be6313a0184d85463fbe3
Ghostfish21 <43630149+Ghostfish21@users.noreply.github.com>
2025-03-28 00:34:15 -0400

    添加了 UnityExts

---
+++ Fries/UnityExts.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Fries {
+    public static class UnityExts {
+        public static string getGameObjectPath(this GameObject obj) {
+            string path = obj.name;
+            Transform current = obj.transform;
+            while (current.parent != null) {
+                current = current.parent;
+                path = current.name + "/" + path;
+            }
+
+            return path;
+        }
+    }
+}
