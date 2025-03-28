ec281f0da59284b088c260aa3db4b776a45431f8
Ghostfish21 <43630149+Ghostfish21@users.noreply.github.com>
2025-03-28 00:15:59 -0400

    添加 EditorAppUtils

---
+++ Fries/Inspector/EditorEvents/EditorAppUtils.cs
@@ -0,0 +1,15 @@
+﻿
+# if UNITY_EDITOR
+using UnityEditor;
+# endif
+
+namespace Fries.Inspector.EditorEvents {
+    public class EditorAppUtils {
+        public static double timeSinceStartUp() {
+            # if UNITY_EDITOR
+            return EditorApplication.timeSinceStartup;
+            # endif
+            return -1;
+        }
+    }
+}
