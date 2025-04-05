789bdbeda02b27d4c61b4978cc274b0ee979e192
Ghostfish21 <43630149+Ghostfish21@users.noreply.github.com>
2025-04-04 23:38:33 -0400

    Update FbxIdCalculator.cs

--- Fries/FbxFunctions/FbxId/FbxIdCalculator.cs
+++ Fries/FbxFunctions/FbxId/FbxIdCalculator.cs
@@ -80,10 +80,10 @@ namespace Fries.FbxFunctions.FbxId {
 
                 if (_closeAfterFinish)
                     TaskPerformer.TaskPerformer.executeExe(getExePath("FbxId_py"),
-                        new[] { arg, "1" }, true, false);
+                        new[] { arg, _exportTxtFilePath, "1" }, true, false);
                 else
                     TaskPerformer.TaskPerformer.executeExe(getExePath("FbxId_py"),
-                        new[] { arg }, true, false);
+                        new[] { arg, _exportTxtFilePath }, true, false);
             }
         }
 
