b45f4ae1c476397340d58f5547d4ed316e85e6f3
Ghostfish21 <43630149+Ghostfish21@users.noreply.github.com>
2025-06-07 17:56:57 -0400

    更新 TriggerData

---
+++ Fries/TriggerData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Fries {
+    [RequireComponent(typeof(Collider))]
+    public class TriggerData : MonoBehaviour {
+        public List<GameObject> objects = new();
+        
+        private void OnTriggerEnter(Collider other) {
+            objects.Add(other.gameObject);
+        }
+
+        private void OnTriggerExit(Collider other) {
+            objects.Remove(other.gameObject);
+        }
+    }
+}
