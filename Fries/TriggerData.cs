385d094a60bbb0cb1769bc7dfad7fc1c9f344a08
Ghostfish21 <43630149+Ghostfish21@users.noreply.github.com>
2025-06-07 20:04:05 -0400

    Update TriggerData.cs

--- Fries/TriggerData.cs
+++ Fries/TriggerData.cs
@@ -5,14 +5,45 @@ using UnityEngine;
 namespace Fries {
     [RequireComponent(typeof(Collider))]
     public class TriggerData : MonoBehaviour {
+        public string checkTag;
         public List<GameObject> objects = new();
-        
+        private List<GameObject> checkingList = new();
+
+        private bool shouldCheckObjects = false;
+        private bool checkFlag = false;
+
+        private void Awake() {
+            TaskPerformer.TaskPerformer.inst()
+                .scheduleRepeatingTask((Action)(() => { shouldCheckObjects = true; }), 2f);
+        }
+
         private void OnTriggerEnter(Collider other) {
+            if (!other.gameObject.hasTag(checkTag)) return;
             objects.Add(other.gameObject);
         }
 
+        private void OnTriggerStay(Collider other) {
+            if (!checkFlag) return;
+            if (!other.gameObject.hasTag(checkTag)) return;
+            checkingList.Add(other.gameObject);
+        }
+
         private void OnTriggerExit(Collider other) {
+            if (!other.gameObject.hasTag(checkTag)) return;
             objects.Remove(other.gameObject);
+        }
+
+        private void FixedUpdate() {
+            if (shouldCheckObjects) {
+                checkFlag = true;
+                shouldCheckObjects = false;
+            }
+
+            else if (checkFlag) {
+                checkFlag = false;
+                objects = checkingList;
+                checkingList = new List<GameObject>();
+            }
         }
     }
 }
