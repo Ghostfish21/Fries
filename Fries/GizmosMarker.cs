8227d7c10792e2d04e888755ba183e39afbd4cab
Ghostfish21 <43630149+Ghostfish21@users.noreply.github.com>
2025-06-16 18:36:59 -0400

    更新 Gizmos

---
+++ Fries/GizmosMarker.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+namespace Fries {
+    public class GizmosMarker : TestMonoBehaviour {
+        public float radiu = 0.1f;
+        private void OnDrawGizmos() {
+            Gizmos.DrawSphere(transform.position, radiu);
+        }
+    }
+}
