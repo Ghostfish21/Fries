3c3a52dd9118c5eba018e4ed17fa72912aefad14
Ghostfish21 <43630149+Ghostfish21@users.noreply.github.com>
2025-06-21 02:07:29 -0400

    Update Yaw.cs

--- Fries/Yaw.cs
+++ Fries/Yaw.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 namespace Fries {
     public class Yaw : MonoBehaviour {
         public float yaw { get; private set; } = 0;
-
+        public float yaw1 = 0;
         private void Update() {
             Vector3 endPoint = transform.TransformPoint(1, 0, 0);
             float f = Mathf.Atan2(endPoint.z, endPoint.x) * Mathf.Rad2Deg;
             if (f > 360) f -= 360;
             else if (f <= 0) f += 360;
             yaw = f;
+            yaw1 = f;
         }
     }
 }
