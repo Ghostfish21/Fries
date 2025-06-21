9f88aea7e5ed9eaebd70fcb6fdcb6b880b1bf17f
Ghostfish21 <43630149+Ghostfish21@users.noreply.github.com>
2025-06-21 03:03:15 -0400

    Update AngleMath.cs

--- Fries/AngleMath.cs
+++ Fries/AngleMath.cs
@@ -7,6 +7,7 @@ namespace Fries {
         // 该计算返回 Atan2(end.z, end.x) 的结果 的 (0, 360] 的值
         public static float getWorldYaw(this Transform transform) {
             Vector3 endPoint = transform.TransformPoint(1, 0, 0);
+            endPoint -= transform.position;
             float f = Mathf.Atan2(endPoint.z, endPoint.x) * Mathf.Rad2Deg;
             if (f > 360) f -= 360;
             else if (f <= 0) f += 360;
