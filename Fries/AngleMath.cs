032d72f17d8b547af3a31b70848763cee49fd9a3
Ghostfish21 <43630149+Ghostfish21@users.noreply.github.com>
2025-06-21 02:46:52 -0400

    简化 Yaw 类

--- Fries/AngleMath.cs
+++ Fries/AngleMath.cs
@@ -2,16 +2,15 @@
 using UnityEngine;
 
 namespace Fries {
-    public class Yaw : MonoBehaviour {
-        public float yaw { get; private set; } = 0;
-        public float yaw1 = 0;
-        private void Update() {
+    public static class AngleMath {
+        // 获取 Transform 在世界坐标系下计算的 偏航角（Yaw）
+        // 该计算返回 Atan2(end.z, end.x) 的结果 的 (0, 360] 的值
+        public static float getWorldYaw(this Transform transform) {
             Vector3 endPoint = transform.TransformPoint(1, 0, 0);
             float f = Mathf.Atan2(endPoint.z, endPoint.x) * Mathf.Rad2Deg;
             if (f > 360) f -= 360;
             else if (f <= 0) f += 360;
-            yaw = f;
-            yaw1 = f;
+            return f;
         }
     }
 }
