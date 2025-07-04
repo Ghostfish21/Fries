9db46ea07be71238fdc375db29512f1320c74f9d
Ghostfish21 <43630149+Ghostfish21@users.noreply.github.com>
2025-07-04 02:12:48 -0400

    Update EventFunctionSubscriber.cs

--- Fries/EventFunctions/EventFunctionSubscriber.cs
+++ Fries/EventFunctions/EventFunctionSubscriber.cs
@@ -1,12 +1,17 @@
 ﻿namespace Fries.EventFunctions {
-    public interface EventFunctionSubscriber { }
+    public interface EventFunctionSubscriber {
+        void onSubscribe();
+        void onUnsubscribe();
+    }
 
     public static class EventFunctionSubscriberExtensions {
         public static void subscribe(this EventFunctionSubscriber subscriber) {
             EventFunctionSystem.inst.record(subscriber);
+            subscriber.onSubscribe();
         }
         public static void unsubscribe(this EventFunctionSubscriber subscriber) {
             EventFunctionSystem.inst.remove(subscriber);
+            subscriber.onUnsubscribe();
         }
     }
 }
