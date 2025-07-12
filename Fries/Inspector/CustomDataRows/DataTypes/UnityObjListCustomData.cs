3fd073c7eb4b0625deeee06f3b8e14aa51e8b3c8
Ghostfish21 <43630149+Ghostfish21@users.noreply.github.com>
2025-07-12 18:26:41 -0400

    更新 CustomData 对 Unity.Object List 的支持

---
+++ Fries/Inspector/CustomDataRows/DataTypes/UnityObjListCustomData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Fries.Inspector.CustomDataRows {
+    public class UnityObjListCustomData : CustomDataType {
+
+        public string getDisplayName() {
+            return "List<Unity.Object>";
+        }
+
+        public Type getType() {
+            return typeof(UnityObjListWrapper);
+        }
+
+        public object getDefaultValue() {
+            return new UnityObjListWrapper();
+        }
+    }
+}
---
+++ Fries/Inspector/CustomDataRows/DataTypes/UnityObjListWrapper.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Fries.Inspector.CustomDataRows {
+    public class UnityObjListWrapper : Unwrapper {
+        public List<Object> list = new();
+        public object unwrap() {
+            return list;
+        }
+    }
+}
