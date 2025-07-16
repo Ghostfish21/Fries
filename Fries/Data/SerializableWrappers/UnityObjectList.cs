099df98898f30c070acce1be838214b2d938e5d9
Ghostfish21 <43630149+Ghostfish21@users.noreply.github.com>
2025-07-15 23:01:17 -0400

    Update UnityObjectList.cs

--- Fries/Data/SerializableWrappers/UnityObjectList.cs
+++ Fries/Data/SerializableWrappers/UnityObjectList.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using Object = UnityEngine.Object;
 
 namespace Fries.Data.SerializableWrappers {
+    [Serializable]
     public class UnityObjectList : SerializableWrapper {
         public List<Object> v;
         public override T get<T>() {
