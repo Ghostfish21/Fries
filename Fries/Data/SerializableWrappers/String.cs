9c999928d8b85e7a9c340e4066a7d83dc0db7835
Ghostfish21 <43630149+Ghostfish21@users.noreply.github.com>
2025-08-03 23:31:55 -0400

    更新 SerializedWrapper 的 String类

---
+++ Fries/Data/SerializableWrappers/String.cs
@@ -0,0 +1,13 @@
+﻿namespace Fries.Data.SerializableWrappers {
+    public class String : SerializableWrapper {
+        public string v;
+        
+        public override T get<T>() {
+            return (T)(object)v;
+        }
+
+        public override object get() {
+            return v;
+        }
+    }
+}
