ad87cd49a4252025d08bdd311d66eec85e4af42d
Ghostfish21 <43630149+Ghostfish21@users.noreply.github.com>
2025-10-12 23:18:25 -0400

    添加 CustomTriggerData

--- Fries/CustomTriggerData.cs
+++ Fries/CustomTriggerData.cs
@@ -4,12 +4,12 @@ using System.Linq;
 using UnityEngine;
 
 namespace Fries {
-    
     [RequireComponent(typeof(Collider))]
-    public class TriggerData : MonoBehaviour {
+    public class CustomTriggerData : MonoBehaviour {
         public float refreshRate = 2f;
+
+        public Func<Collider, bool> isObjectValid;
         
-        public List<string> checkTags;
         public List<GameObject> objects = new();
         private List<GameObject> checkingList = new();
 
@@ -23,18 +23,24 @@ namespace Fries {
         
 
         private void OnTriggerEnter(Collider other) {
-            if (!checkTags.Any(other.gameObject.hasTag)) return;
+            if (isObjectValid == null) return;
+            
+            if (!isObjectValid(other)) return;
             objects.Add(other.gameObject);
         }
 
         private void OnTriggerStay(Collider other) {
+            if (isObjectValid == null) return;
+            
             if (!checkFlag) return;
-            if (!checkTags.Any(other.gameObject.hasTag)) return;
+            if (!isObjectValid(other)) return;
             checkingList.Add(other.gameObject);
         }
 
         private void OnTriggerExit(Collider other) {
-            if (!checkTags.Any(other.gameObject.hasTag)) return;
+            if (isObjectValid == null) return;
+            
+            if (!isObjectValid(other)) return;
             objects.Remove(other.gameObject);
         }
 
