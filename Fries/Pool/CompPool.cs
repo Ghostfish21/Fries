3c0441276cef6f8710a4d8afdbf24e8de411ed53
Ghostfish21 <43630149+Ghostfish21@users.noreply.github.com>
2025-10-25 11:10:55 -0400

    Update CompPool.cs

--- Fries/Pool/CompPool.cs
+++ Fries/Pool/CompPool.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 namespace Fries.Pool {
     public class CompPool<T> : APool<T> where T : Component {
+        private Transform root;
         
         public CompPool(Action<T> compSetup, Action<T> resetter, Transform root, GameObject prefab, int size = 5) : base(() => {
             T comp = GameObject.Instantiate(prefab, root).GetComponent<T>();
@@ -10,6 +11,7 @@ namespace Fries.Pool {
             compSetup(comp);
             return comp;
         }, size) {
+            this.root = root;
             this.resetter = resetter;
         }
         
@@ -18,6 +20,7 @@ namespace Fries.Pool {
             comp.gameObject.SetActive(false);
             return comp;
         }, size) {
+            this.root = root;
             this.resetter = resetter;
         }
         
@@ -26,11 +29,13 @@ namespace Fries.Pool {
             comp.gameObject.SetActive(false);
             return comp;
         }, size) {
+            this.root = root;
             this.resetter = _ => {};
         }
 
         protected override void deactivateCore(T what) {
             what.gameObject.SetActive(false);
+            what.transform.SetParent(root);
         }
 
         protected override void activateCore(T what) {
