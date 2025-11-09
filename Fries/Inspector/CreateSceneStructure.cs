f4ac60692ab22d8113110382ee655e1d86aa6d80
Ghostfish21 <43630149+Ghostfish21@users.noreply.github.com>
2025-11-09 16:27:30 -0500

    更新新建场景快捷方法与父级设置器

---
+++ Fries/Inspector/CreateSceneStructure.cs
@@ -0,0 +1,18 @@
+﻿# if UNITY_EDITOR
+using UnityEditor;
+# endif
+using UnityEngine;
+
+namespace Fries.Inspector {
+    public class CreateSceneStructure {
+# if UNITY_EDITOR
+        [MenuItem("Fries/Create Scene Structure")]
+        public static void Create() {
+            new GameObject("==== System ====");
+            new GameObject("==== Scene   ====");
+            new GameObject("==== Render  ====");
+            new GameObject("==== Ui          ====");
+        }
+# endif
+    }
+}
---
+++ Fries/ParentSetter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+namespace Fries {
+    public class ParentSetter : MonoBehaviour {
+        public Transform parent;
+        private void Awake() {
+            transform.SetParent(parent);
+        }
+    }
+}
