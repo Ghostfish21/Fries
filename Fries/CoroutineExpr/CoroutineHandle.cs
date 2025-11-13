3f93bc806f588be3de250e90fd20c776c4a2d6a4
Ghostfish21 <43630149+Ghostfish21@users.noreply.github.com>
2025-11-12 19:38:38 -0500

    更新 CoroutineScheduler

---
+++ Fries/CoroutineExpr/CoroutineHandle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+
+namespace Fries.CoroutineExpr {
+    public class CoroutineHandle {
+        public IEnumerator routine { get; private set; }
+        private bool isComplete;
+        public Exception fault;
+
+        public CoroutineHandle(IEnumerator routine) {
+            this.routine = routine;
+        }
+
+        public const int NEW = 0;
+        public const int RUNNING = 1;
+        public const int SUSPENDED = 2;
+        public const int COMPLETED = 3;
+        public const int FAULTED = 4;
+        private int status = NEW;
+
+        public bool isRegistrable() {
+            if (status is NEW or SUSPENDED) return true;
+            return false;
+        }
+        public bool isSuspendable() {
+            if (status is RUNNING) return true;
+            return false;
+        }
+
+        public void setStatus(int status) {
+            this.status = status;
+        }
+    }
+}
---
+++ Fries/CoroutineExpr/CoroutineScheduler.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Fries.CoroutineExpr {
+    public class CoroutineScheduler : MonoBehaviour {
+        private List<CoroutineHandle> runningCoroutines = new();
+        private readonly object lockObj = new();
+
+        private void Awake() {
+            DontDestroyOnLoad(gameObject);
+        }
+
+        private CoroutineHandle start(IEnumerator routine) {
+            CoroutineHandle handle;
+            lock (lockObj) {
+                handle = new CoroutineHandle(routine);
+                runningCoroutines.Add(handle);
+                handle.setStatus(CoroutineHandle.RUNNING);
+            }
+            return handle;
+        }
+
+        private void suspend(CoroutineHandle handle) {
+            lock (lockObj) {
+                if (!handle.isSuspendable()) return;
+                handle.setStatus(CoroutineHandle.SUSPENDED);
+                runningCoroutines.Remove(handle);
+            }
+        }
+
+        private void resume(CoroutineHandle handle) {
+            lock (lockObj) {
+                if (!handle.isRegistrable()) return;
+                handle.setStatus(CoroutineHandle.RUNNING);
+                runningCoroutines.Add(handle);
+            }
+        }
+        
+        private void Update() {
+            lock (lockObj) {
+                for (int i = runningCoroutines.Count-1; i >= 0; i--) {
+                    CoroutineHandle handle = runningCoroutines[i];
+                    try {
+                        Func<bool> isReady = null;
+                        if (handle.routine.Current != null) {
+                            object y = handle.routine.Current;
+                            if (y is Func<bool> predicate) isReady = predicate;
+                            else Debug.LogWarning("Coroutine predicate must be a function, skipping...");
+                        }
+                        if (isReady != null && !isReady()) continue;
+                    
+                        if (handle.routine.MoveNext()) continue;
+                        runningCoroutines.RemoveAt(i); 
+                        handle.setStatus(CoroutineHandle.COMPLETED);
+                    }
+                    catch (Exception ex) {
+                        runningCoroutines.RemoveAt(i);
+                        handle.fault = ex;
+                        handle.setStatus(CoroutineHandle.FAULTED);
+                        Debug.LogException(ex);
+                    }
+                }
+            }
+        }
+        
+        
+    }
+}
