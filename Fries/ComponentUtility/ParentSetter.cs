ceb47c7f8ff55cf18dcb4b3f9002f312b5b4862c
Ghostfish21 <43630149+Ghostfish21@users.noreply.github.com>
2025-11-13 11:30:10 -0500

    Update ParentSetter.cs

--- Fries/ComponentUtility/ParentSetter.cs
+++ Fries/ComponentUtility/ParentSetter.cs
@@ -3,10 +3,31 @@ using UnityEngine;
 
 namespace Fries {
     public class ParentSetter : MonoBehaviour {
+        public const int AWAKE = -2;
+        public const int START = -1;
+        
         public Transform parent;
+        public int timing = -2;
         private void Awake() {
+            if (timing < AWAKE) timing = AWAKE;
+            if (timing != AWAKE) return;
             transform.SetParent(parent);
             Destroy(this);
+        }
+        
+        private void Start() {
+            if (timing != START) return;
+            transform.SetParent(parent);
+            Destroy(this);
+        }
+
+        private int updateCount = 0;
+        private void Update() {
+            if (updateCount == timing) {
+                transform.SetParent(parent);
+                Destroy(this);
+            }
+            updateCount++;
         }
     }
 }
