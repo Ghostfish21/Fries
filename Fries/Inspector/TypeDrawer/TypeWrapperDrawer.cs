fd6b77de6307c8a1c57bf3f3eea4f216c731d639
Ghostfish21 <43630149+Ghostfish21@users.noreply.github.com>
2025-11-24 14:10:28 -0500

    Update TypeWrapperDrawer.cs

--- Fries/Inspector/TypeDrawer/TypeWrapperDrawer.cs
+++ Fries/Inspector/TypeDrawer/TypeWrapperDrawer.cs
@@ -7,13 +7,14 @@ namespace Fries.Inspector.TypeDrawer {
     public class TypeWrapperDrawer : PropertyDrawer {
         // 绘制所有 public 选项后，如果选项为空，再单独绘制一个窗口用于拖拽monoscript，拽进去后捕获值到 TypeWrapper 中
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
-            EditorGUI.BeginProperty(position, label, property);
-            SerializedProperty sp = property.FindPropertyRelative("typeToString");
-            EditorGUI.PropertyField(position, property.FindPropertyRelative("assemblyName"), GUIContent.none);
-            EditorGUI.PropertyField(position, property.FindPropertyRelative("typeNameIncludeNamespace"), GUIContent.none);
-            EditorGUI.PropertyField(position, sp, GUIContent.none);
+            EditorGUILayout.BeginVertical();
             
-            EditorGUI.EndProperty();
+            SerializedProperty sp = property.FindPropertyRelative("typeToString");
+            EditorGUILayout.PropertyField(property.FindPropertyRelative("assemblyName"));
+            EditorGUILayout.PropertyField(property.FindPropertyRelative("typeNameIncludeNamespace"));
+            EditorGUILayout.PropertyField(sp);
+            
+            EditorGUILayout.EndVertical();
         }
     }
 }
