ffd1ea2476fe6ea9bdf5be3d9b92a1898e9567b0
Ghostfish21 <43630149+Ghostfish21@users.noreply.github.com>
2025-11-24 14:43:09 -0500

    DEBUG

--- Fries/Inspector/TypeDrawer/TypeWrapper.cs
+++ Fries/Inspector/TypeDrawer/TypeWrapper.cs
@@ -1,31 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
 using UnityEngine.Analytics;
 
 namespace Fries.Inspector.TypeDrawer {
     [Serializable]
     public class TypeWrapper {
         public string assemblyName;
-        public string typeNameIncludeNamespace;
+        public List<string> typeNamesIncludeNamespace;
         public string typeToString;
         private long prevFindTime;
         private Type type;
+
+        // 这个方法仅支持在 Editor 中运行
+        public void fillInByFile(string assemblyName, string fullPath2File) {
+# if UNITY_EDITOR
+            string scriptContent = File.ReadAllText(fullPath2File);
+            
+# else
+            throw new Exception("The fillInByFile method only works in Editor!");
+# endif
+        }
         
-        public void setType(string assemblyName, string typeNameIncludeNamespace) {
+        private void setType(string assemblyName, List<string> typeNamesIncludeNamespace) {
             this.assemblyName = assemblyName;
-            this.typeNameIncludeNamespace = typeNameIncludeNamespace;
+            // this.typeNameIncludeNamespace = typeNameIncludeNamespace;
             findType();
         }
 
         private void findType() {
-            if (prevFindTime == AnalyticsSessionInfo.sessionId) return;
-            prevFindTime = AnalyticsSessionInfo.sessionId;
-            foreach (var asm in AppDomain.CurrentDomain.GetAssemblies()) {
-                if (asm.GetName().Name != assemblyName) continue;
-                type = asm.GetType(typeNameIncludeNamespace);
-                break;
-            }
-            if (type == null) typeToString = "";
-            typeToString = type?.ToString();
+            // if (prevFindTime == AnalyticsSessionInfo.sessionId) return;
+            // prevFindTime = AnalyticsSessionInfo.sessionId;
+            // foreach (var asm in AppDomain.CurrentDomain.GetAssemblies()) {
+            //     if (asm.GetName().Name != assemblyName) continue;
+            //     type = asm.GetType(typeNameIncludeNamespace);
+            //     break;
+            // }
+            // if (type == null) typeToString = "";
+            // typeToString = type?.ToString();
         }
         
         public Type getType() {
--- Fries/Inspector/TypeDrawer/TypeWrapperDrawer.cs
+++ Fries/Inspector/TypeDrawer/TypeWrapperDrawer.cs
@@ -8,13 +8,16 @@ namespace Fries.Inspector.TypeDrawer {
         // 绘制所有 public 选项后，如果选项为空，再单独绘制一个窗口用于拖拽monoscript，拽进去后捕获值到 TypeWrapper 中
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
             EditorGUILayout.BeginVertical();
-
-            EditorGUILayout.LabelField(property.displayName);
-            SerializedProperty sp = property.FindPropertyRelative("typeToString");
-            EditorGUILayout.PropertyField(property.FindPropertyRelative("assemblyName"));
-            EditorGUILayout.PropertyField(property.FindPropertyRelative("typeNameIncludeNamespace"));
-            EditorGUILayout.PropertyField(sp);
             
+            SerializedProperty t2s = property.FindPropertyRelative("typeToString");
+            SerializedProperty assemblyName = property.FindPropertyRelative("assemblyName");
+            SerializedProperty typeNameIncludeNamespace = property.FindPropertyRelative("typeNameIncludeNamespace");
+            EditorGUILayout.LabelField(property.displayName + $" {t2s.stringValue}");
+            if (string.IsNullOrEmpty(t2s.stringValue)) {
+                MonoScript script = (MonoScript)EditorGUILayout.ObjectField("Class", null, typeof(MonoScript), false);
+                Debug.Log(script.text);
+            }
+
             EditorGUILayout.EndVertical();
         }
     }
