0a7f43940e3a43c17dea65f4f11a41753f7a1a74
Ghostfish21 <43630149+Ghostfish21@users.noreply.github.com>
2025-11-30 01:00:22 -0500

    更新小工具

---
+++ Fries/ComponentUtility/SetInactive.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Fries {
+    public class SetInactive : MonoBehaviour {
+        public const int AWAKE = -2;
+        public const int START = -1;
+        
+        public int timing = -2;
+        private void Awake() {
+            if (timing < AWAKE) timing = AWAKE;
+            if (timing != AWAKE) return;
+            gameObject.SetActive(false);
+            Destroy(this);
+        }
+        
+        private void Start() {
+            if (timing != START) return;
+            gameObject.SetActive(false);
+            Destroy(this);
+        }
+
+        private int updateCount = 0;
+        private void Update() {
+            if (updateCount == timing) {
+                gameObject.SetActive(false);
+                Destroy(this);
+            }
+            updateCount++;
+        }
+    }
+}
