47f7166d77e984d5caa3989da895979fa66befae
Ghostfish21 <43630149+Ghostfish21@users.noreply.github.com>
2025-11-29 22:32:42 -0500

    Update ChatController.cs

--- Fries/Chat/Ui/ChatController.cs
+++ Fries/Chat/Ui/ChatController.cs
@@ -1,14 +1,12 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using Fries.EvtSystem;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.Scripting;
 
 namespace Fries.Chat.Ui {
-    public class ChatController : MonoBehaviour {
+    public class ChatController : MonoBehaviour, IDeselectHandler {
         private static ChatCore.Writer writer;
         [EvtListener(typeof(ChatCore.OnInitiated))]
         private static void onChatInitiated() {
@@ -18,6 +16,9 @@ namespace Fries.Chat.Ui {
         public static void onMsgPrinted(Message message) {
             Debug.Log(message.content);
         }
+        
+        [EvtDeclarer] public struct OnChatboxOpened {}
+        [EvtDeclarer] public struct OnChatboxClosed {}
         
         private bool isChatboxOpen = false;
         
@@ -29,6 +30,12 @@ namespace Fries.Chat.Ui {
         }
         private void Update() {
             if (isChatboxOpen) {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+
+                if (Input.GetMouseButtonDown(0)) 
+                    moveCaretToMovePosX();
+                
                 if (Input.GetKeyDown(KeyCode.Return)) {
                     if (writer == null) 
                         throw new InvalidOperationException(
@@ -37,6 +44,10 @@ namespace Fries.Chat.Ui {
                     writer.write(inputField.text);
                     inputField.text = "";
                     isChatboxOpen = false;
+                    
+                    Cursor.lockState = CursorLockMode.Locked;
+                    Cursor.visible = false;
+                    Evt.TriggerNonAlloc<OnChatboxClosed>();
                 }
                 else if (isChatboxOpen && Input.GetKeyDown(KeyCode.Escape)) {
                     inputField.text = "";
@@ -46,6 +57,7 @@ namespace Fries.Chat.Ui {
                     
                     Cursor.lockState = CursorLockMode.Locked;
                     Cursor.visible = false;
+                    Evt.TriggerNonAlloc<OnChatboxClosed>();
                 }
             }
 
@@ -54,6 +66,10 @@ namespace Fries.Chat.Ui {
                     isChatboxOpen = true;
                     EventSystem.current.SetSelectedGameObject(inputField.gameObject, null);
                     inputField.ActivateInputField();
+                    
+                    Cursor.lockState = CursorLockMode.None;
+                    Cursor.visible = true;
+                    Evt.TriggerNonAlloc<OnChatboxOpened>();
                 }
                 else if (Input.GetKeyDown(KeyCode.Slash)) {
                     isChatboxOpen = true;
@@ -61,19 +77,55 @@ namespace Fries.Chat.Ui {
                     inputField.ActivateInputField();
                     inputField.text = "/";
                     
+                    Cursor.lockState = CursorLockMode.None;
+                    Cursor.visible = true;
                     StartCoroutine(MoveCaretToEnd());
+                    Evt.TriggerNonAlloc<OnChatboxOpened>();
                 }
             }
         }
 
+        private const float FastCaretRate = 4;
+        private const float SlowCaretRate = 0.85f;
+
         private static readonly WaitForEndOfFrame wait = new();
         private IEnumerator MoveCaretToEnd() {
             yield return wait;
+            inputField.caretBlinkRate = FastCaretRate;
             
             inputField.caretPosition = 1;
             inputField.selectionStringAnchorPosition = 1;
             inputField.selectionStringFocusPosition  = 1;
+
+            yield return wait;
+            inputField.caretBlinkRate = SlowCaretRate;
         }
 # endif
+        
+        public void OnDeselect(BaseEventData eventData) {
+            if (!isChatboxOpen) return;
+            StartCoroutine(refocus());
+        }
+
+        private IEnumerator refocus() {
+            yield return wait;
+            EventSystem.current.SetSelectedGameObject(null);
+        }
+
+        private void moveCaretToMovePosX() {
+            Vector3 mousePos = Input.mousePosition;
+            TMP_Text text = inputField.textComponent;
+            int charIndex = TMP_TextUtilities.GetCursorIndexFromPosition(text, mousePos, null);
+
+            if (charIndex == -1) {
+                Vector3 textPos = text.transform.position;
+                if (mousePos.x > Camera.main.WorldToScreenPoint(textPos).x) 
+                    charIndex = inputField.text.Length;
+                else charIndex = 0;
+            }
+            
+            inputField.caretPosition = charIndex;
+            inputField.ForceLabelUpdate();
+        }
     }
 }
