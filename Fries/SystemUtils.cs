638cdd3defe1b76bccaf6fbdbedb9aaa22673047
Ghostfish21 <43630149+Ghostfish21@users.noreply.github.com>
2025-12-28 18:16:21 +0800

    Update SystemUtils.cs

--- Fries/SystemUtils.cs
+++ Fries/SystemUtils.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using UnityEditor;
@@ -17,10 +18,7 @@ namespace Fries {
         }
         
         public static bool hasAnnotation(this FieldInfo field, Type type) {
-            if (field == null || type == null)
-                return false;
-
-            // 判断该字段上是否定义了指定的特性（不搜索继承链）
+            if (field == null || type == null) return false;
             return Attribute.IsDefined(field, type, false);
         }
 
@@ -29,6 +27,37 @@ namespace Fries {
             first.CopyTo(joined, 0);
             second.CopyTo(joined, first.Length);
             return joined;
+        }
+
+        private static Dictionary<string, long> _cache = new();
+
+        public static long propertyToId(string name) {
+            if (name is null) throw new ArgumentNullException(nameof(name));
+
+            if (_cache.TryGetValue(name, out var id)) 
+                return id;
+
+            long computed = fnv1a64_Utf16(name);
+            _cache[name] = computed;
+            return computed;
+        }
+
+        private static long fnv1a64_Utf16(string s) {
+            const ulong offset = 14695981039346656037UL;
+            const ulong prime = 1099511628211UL;
+
+            ulong hash = offset;
+
+            unchecked {
+                foreach (ushort ch in s) {
+                    hash ^= (byte)(ch & 0xFF);
+                    hash *= prime;
+
+                    hash ^= (byte)(ch >> 8);
+                    hash *= prime;
+                }
+            }
+            return unchecked((long)hash);
         }
     }
 }
