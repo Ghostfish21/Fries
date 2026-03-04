b7dc4ccc6925947223bae29a15470f456f3bcbd6
Ghostfish21 <43630149+Ghostfish21@users.noreply.github.com>
2026-03-05 02:31:21 +0800

    Update LevelSaver.cs

--- Fries/BlockGrid/LevelEdit/LevelSaver.cs
+++ Fries/BlockGrid/LevelEdit/LevelSaver.cs
@@ -11,60 +11,84 @@ namespace Fries.BlockGrid.LevelEdit {
         public static void Save(GameObject gameObject, string targetFolder, string saveName, bool forceSave = false) {
             # if UNITY_EDITOR
             if (!forceSave) regularSave(gameObject, targetFolder, saveName);
-            else fourceSave(gameObject, targetFolder, saveName);
+            else LevelSaver.forceSave(gameObject, targetFolder, saveName);
             # endif
         }
 
         private static void regularSave(GameObject gameObject, string targetFolder, string saveName) {
-            # if UNITY_EDITOR
+# if UNITY_EDITOR
             LevelEditor.Inst.BlockMap.ClearAllInactives();
-            
+
             TaskPerformer.TaskPerformer.inst().scheduleTask((Action)(() => {
+                bool reload = false;
+
+                // Ensure target folder exists
                 if (!AssetDatabase.IsValidFolder("Assets/" + targetFolder)) {
                     Directory.CreateDirectory("Assets/" + targetFolder);
-                    AssetDatabase.Refresh();
+                    reload = true;
                 }
 
+                // Build paths
                 string fileName = $"{saveName}-{gameObject.name}";
                 string safeName = string.Join("_", fileName.Split(Path.GetInvalidFileNameChars()));
                 string path = $"{targetFolder}/{safeName}.prefab";
-                
+
                 string safeName1 = string.Join("_", saveName.Split(Path.GetInvalidFileNameChars()));
-                var dateStr = DateTime.Now.ToString("yyyyMMddHHmmssfffffff", System.Globalization.CultureInfo.InvariantCulture);
+                var dateStr = DateTime.Now.ToString(
+                    "yyyyMMddHHmmssfffffff",
+                    System.Globalization.CultureInfo.InvariantCulture
+                );
                 string path1 = $"{targetFolder}/{safeName1}/{safeName}-{dateStr}.prefab";
-                
+
+                // Ensure version folder exists (targetFolder/saveName)
+                if (!AssetDatabase.IsValidFolder("Assets/" + $"{targetFolder}/{safeName1}")) {
+                    Directory.CreateDirectory("Assets/" + $"{targetFolder}/{safeName1}");
+                    reload = true;
+                }
+
+                if (reload) AssetDatabase.Refresh();
+
+                // Save
                 PrefabUtility.SaveAsPrefabAsset(gameObject, "Assets/" + path);
                 PrefabUtility.SaveAsPrefabAsset(gameObject, "Assets/" + path1);
 
-                Debug.Log($"Level has been saved at: {path}");
-                EditorGUIUtility.PingObject(AssetDatabase.LoadAssetAtPath<Object>(path));
-                EditorGUIUtility.PingObject(AssetDatabase.LoadAssetAtPath<Object>(path1));
+                Debug.Log($"Level has been saved at: Assets/{path}");
+                EditorGUIUtility.PingObject(AssetDatabase.LoadAssetAtPath<Object>("Assets/" + path));
+                EditorGUIUtility.PingObject(AssetDatabase.LoadAssetAtPath<Object>("Assets/" + path1));
             }), 1);
-            # endif
+# endif
         }
 
-        private static void fourceSave(GameObject gameObject, string targetFolder, string saveName) {
-            # if UNITY_EDITOR
+        private static void forceSave(GameObject gameObject, string targetFolder, string saveName) {
+# if UNITY_EDITOR
+            bool reload = false;
             if (!AssetDatabase.IsValidFolder("Assets/" + targetFolder)) {
                 Directory.CreateDirectory("Assets/" + targetFolder);
-                AssetDatabase.Refresh();
+                reload = true;
             }
 
+    
             string fileName = $"{saveName}-{gameObject.name}";
             string safeName = string.Join("_", fileName.Split(Path.GetInvalidFileNameChars()));
             string path = $"{targetFolder}/{safeName}.prefab";
-            
+    
             string safeName1 = string.Join("_", saveName.Split(Path.GetInvalidFileNameChars()));
             var dateStr = DateTime.Now.ToString("yyyyMMddHHmmssfffffff", System.Globalization.CultureInfo.InvariantCulture);
             string path1 = $"{targetFolder}/{safeName1}/{safeName}-{dateStr}.prefab";
-            
+    
+            if (!AssetDatabase.IsValidFolder("Assets/" + $"{targetFolder}/{safeName1}")) {
+                Directory.CreateDirectory("Assets/" + $"{targetFolder}/{safeName1}");
+                reload = true;
+            }
+            if (reload) AssetDatabase.Refresh();
+    
             PrefabUtility.SaveAsPrefabAsset(gameObject, "Assets/" + path);
             PrefabUtility.SaveAsPrefabAsset(gameObject, "Assets/" + path1);
 
-            Debug.Log($"Level has been saved at: {path}");
-            EditorGUIUtility.PingObject(AssetDatabase.LoadAssetAtPath<Object>(path));
-            EditorGUIUtility.PingObject(AssetDatabase.LoadAssetAtPath<Object>(path1));
-            # endif
+            Debug.Log($"Level has been saved at: Assets/{path}");
+            EditorGUIUtility.PingObject(AssetDatabase.LoadAssetAtPath<Object>("Assets/" + path));
+            EditorGUIUtility.PingObject(AssetDatabase.LoadAssetAtPath<Object>("Assets/" + path1));
+# endif
         }
     }
 }
