0536078f94553ee4a36aa6751fd9de1304d79bdc
Ghostfish21 <43630149+Ghostfish21@users.noreply.github.com>
2026-04-13 05:29:27 +0800

    更新

--- Fries/GobjPersistObjects/GobjSrcgenTarget.cs
+++ Fries/GobjPersistObjects/GobjSrcgenTarget.cs
@@ -2,7 +2,9 @@ using System;
 
 namespace Fries.GobjPersistObjects {
     [AttributeUsage(AttributeTargets.Class, Inherited = false)]
-    public class GobjSrcgenTarget : Attribute { }
+    public class GobjSrcgenTarget : Attribute {
+        public GobjSrcgenTarget(bool areFieldsPersistByDefault = false) {}
+    }
     [AttributeUsage(AttributeTargets.Field)]
     public class NonPersist : Attribute { }
     [AttributeUsage(AttributeTargets.Field)]
